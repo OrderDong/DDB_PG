ad9ca44960e8a59615bca57de650a46dee7d078f
lwd <lwd@qq.com>
2018-05-14 17:13:24 +0800

    登录注册及公共

--- js/app.js
+++ js/app.js
@@ -9,7 +9,6 @@ window.App = {
 					return;
 				}
 				if(accs.length == 0) {
-					alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
 					var installId = document.getElementById("installId");
 					var htmlContent = '<p style="font-size: 20px;">您的MetaMask被锁住了</p><br>'+
 								'<p style="font-size: 12px;color: #545151;">只需打开MetaMask并按照说明进行解锁即可。</p>'+
@@ -53,14 +52,9 @@ window.App = {
 }
 
 window.addEventListener('load', function() {
-  // Checking if Web3 has been injected by the browser (Mist/MetaMask)
   if (typeof web3 !== 'undefined') {
-    console.warn("Using web3 detected from external source. If you find that your accounts don't appear or you have 0 MetaCoin, ensure you've configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask")
-    // Use Mist/MetaMask's provider
     console.log("into net web3");
     window.web3 = new Web3(web3.currentProvider);
-    //window.web3 = new Web3(new Web3.providers.WebsocketProvider("wss://mainnet.infura.io/ws"));
-
       web3.version.getNetwork(function(err, netId){
           switch (netId) {
           case "1":
@@ -83,9 +77,7 @@ window.addEventListener('load', function() {
           }
       })
   } else {
-    console.warn("No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
     console.log("intall matemask");
-    //window.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:9545"));
     var installId = document.getElementById("installId");
 	var htmlContent = '<p style="font-size: 14px;color: #545151;">'+
 				'您将需要一个安全的地方来储存您强悍的蛋蛋！'+
---
+++ js/common.js
@@ -0,0 +1,55 @@
+var auctionAddr = "0x201EfdE2733beE048fCE6368D01A074F30a6F456";
+var tokenAddr = "0x4195E850A8504ef0Cc184Ac1FC22B5Ee5AF0321B";
+var actionInstance;
+
+var account = web3.eth.accounts[0];
+var accountInterval = setInterval(function() {
+  if (web3.eth.accounts[0] !== account) {
+    account = web3.eth.accounts[0];
+  }
+}, 100);
+
+function initContractDanDan(danAbi){
+	var MyContract = web3.eth.contract(danAbi);
+	actionInstance = MyContract.at(tokenAddr);
+	return actionInstance;
+}
+
+function initContractAuction(salesAbi){
+	var MyContract = web3.eth.contract(salesAbi);
+	actionInstance = MyContract.at(auctionAddr);
+	return actionInstance;
+}
+
+window.addEventListener('load', function() {
+  if (typeof web3 !== 'undefined') {
+    console.warn("Using web3 detected from external source. If you find that your accounts don't appear or you have 0 MetaCoin, ensure you've configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask")
+    console.log("into net web3");
+    window.web3 = new Web3(web3.currentProvider);
+	  web3.version.getNetwork(function(err, netId){
+	      switch (netId) {
+	      case "1":
+	          console.log('This is mainnet')
+	          break
+	      case "2":
+	          console.log('This is the deprecated Morden test network.')
+	          break
+	      case "3":
+	          console.log('This is the ropsten test network.')
+	          break
+	      case "4":
+	          console.log('This is the Rinkeby test network.')
+	          break
+	      case "42":
+	          console.log('This is the Kovan test network.')
+	          break
+	      default:
+	          console.log('This is an unknown network.')
+	      }
+     });
+  } else {
+    console.warn("No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
+    console.log("intall matemask");
+    location.href = "./../sign.html";
+  }
+});
