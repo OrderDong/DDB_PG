d0a5eb3e57c120a1e974ce6bfcf69d340f57f19f
huamoai <879619544@qq.com>
2018-05-21 16:38:15 +0800

    我的宝贝创建拍卖

--- js/mydd.js
+++ js/mydd.js
@@ -34,6 +34,12 @@ eggCardInstance.tokensOfOwner(account, function (error, result) {
             myCardArr[j] = cardObj;
             j++;
 
+            $("#eggCardAuction").append('<div onclick="sellBB(' + cardObj.cardId + ')" class="mydd-dd-box">\n' +
+                '                            <div class="mydd-baby-img"><img src="http://t.cn/RCzsdCq"></div>\n' +
+                '                            <p class="">宝贝名称--' + cardObj.cardId + '</p>\n' +
+                '                        </div>')
+
+
         });
     }
     console.log(myCardArr);
@@ -42,11 +48,36 @@ eggCardInstance.tokensOfOwner(account, function (error, result) {
 
 // createCardAuction("3",0.06);
 
-function createCardAuction(tokenId, price) {
-    var price = web3.toWei(price, 'ether');
-    var expiresAt = new Date().getTime() * 2;
-    eggCardAuctionInstance.createCardAuction.sendTransaction(tokenId, price, expiresAt, function (error, result) {
+function sellBB(cardId) {
+    layer.open({
+        title: '卖出宝贝',
+        type: 1,
+        skin: 'layui-layer-demo', //样式类名
+        closeBtn: 1, //不显示关闭按钮
+        anim: 2,
+        area: ['315px', '240px'],
+        shadeClose: true, //开启遮罩关闭
+        content: '<div class="dandan-modal1">'
+        + '<div class="layui-form-item"><img src="http://t.cn/RCzsdCq" class="dandan-img">'
+        + '    <p style="padding: 3px;"><label class="layui-form-label1">售出价格</label>'
+        + '      <input type="text" name="title" id="bb_price" placeholder="请输入价格" class="layui-input1"> DDC</p>'
+        + '    <p style="padding: 3px;">高出均价：+2.3%</p>'
+        + '    <p style="padding: 3px;">现市场平均价：0.025ETH</p>'
+        + '  </div></div>'
+        , btn: ['确定出售', '取消']
+        , yes: function (index, layero) {
+            var _price = $("#bb_price").val();
+            if (!_price) {
+                layer.msg('请输入价格');
+                return;
+            }
+            var price = web3.toWei(_price, 'ether');
+            var expiresAt = new Date().getTime() * 2;
+            console.log(cardId + "," + price + "," + expiresAt);
+            eggCardAuctionInstance.createCardAuction.sendTransaction(cardId, price, expiresAt, function (error, result) {
 
+            });
+        }
     });
 }
 
